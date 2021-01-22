17f0867abbb64884ffd91d66cc54bdeb05cfb11c
Valeria Cuellar Portella <valeria.cuellar@itsight.pe>
2021-01-21 23:52:22 -0500

    vrc

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -2,16 +2,21 @@
   <div id="bg_blue">
     <b-container class="login_container">
       <img class="logo" src="@/assets/img/logo.png" />
-      <b-form>
+      <b-form @submit="onSubmit">
         <b-form-group style="margin-bottom:70px">
           <b-form-input
             type="number"
-            placeholder="Nombre"
+            placeholder="DNI"
+            v-model="dni"
+            :state="dniState"
             required
+            trim
+            aria-describedby="input-live-help input-live-feedback"
           ></b-form-input>
         </b-form-group>
+        <span v-if="dniState" class="invalid-feedback">Campo requerido</span>
         <b-form-group class="mb-4">
-          <b-button block  variant="outline-light">INGRESAR</b-button>
+          <b-button block  variant="outline-light" @click="$router.push('tarjetas')">INGRESAR</b-button>
         </b-form-group>
         <b-form-group>
           <b-button block variant="outline-light" @click="$router.push('registro')">REGISTRARSE</b-button>
@@ -25,11 +30,19 @@
   export default {
     data() {
       return {
-        
+        dni: null,
+      }
+    },
+    computed: {
+      dniState() {
+        return this.dni ? true : false
       }
     },
     methods: {
-     
+      onSubmit(event) {
+        event.preventDefault()
+        alert(JSON.stringify(this.dni))
+      },
     }
   }
 </script>
